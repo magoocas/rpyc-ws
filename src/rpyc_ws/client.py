1f7f97066b932a90e67c572d408809c9eab6c173
Luke Cummings <luke.cummings@cenovus.com>
2025-05-09 13:40:23 -0600

    feat: update version to 0.2.0, add FastAPI as a dependency, and refactor WebSocket handling in client and server

--- src/rpyc_ws/client.py
+++ src/rpyc_ws/client.py
@@ -1,13 +1,27 @@
-from contextlib import contextmanager
+from functools import wraps
 
 from rpyc.utils.classic import connect_stream
 from websockets.sync.client import connect
 
-from rpyc_ws.stream import WebSocketStream
+from rpyc_ws.stream import CallbackStream
 
 
-@contextmanager
-def connect_ws(uri: str, headers: dict[str, str] = None):
-    with connect(uri, additional_headers=headers) as websocket:
-        stream = WebSocketStream(websocket)
-        yield connect_stream(stream)
+@wraps(connect)
+def connect_ws(*args, **kwargs):
+    websocket = connect(*args, **kwargs)
+
+    def receive_bytes(timeout: float):
+        try:
+            result = websocket.recv(timeout, False)
+            return result
+        except TimeoutError:
+            return None
+
+    def send_bytes(data: bytes):
+        websocket.send(data)
+
+    def close():
+        websocket.close()
+
+    stream = CallbackStream(receive_bytes, send_bytes, close)
+    return connect_stream(stream)
